dc45ad66cce5af42faa5eb88b08ce5ca1e111ac1
Bengt-Erik Fröberg <>
2020-04-26 20:57:21 +0200

    CUR-3 index.html page begun

---
+++ src/main/java/net/benfro/curiae/plannerbackend/web/WebController.java
@@ -0,0 +1,13 @@
+package net.benfro.curiae.plannerbackend.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+   @RequestMapping(value = "/")
+   public String index() {
+      return "index";
+   }
+}
